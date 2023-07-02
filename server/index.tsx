15ee3b68da57ec2f00c3be91cb1f4c5226d2d7d4
Kurtis <kcasperson7@gmail.com>
2023-07-02 18:32:10 -0400

    server request

--- server/index.tsx
+++ server/index.tsx
@@ -12,8 +12,39 @@ app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
 
-// should I use the 'dist' file path for local  development?
-// or use localhost in development?
+
 app.use(cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }))
 
 app.use(express.static(path.join(__dirname, '../../client/dist')))
+
+
+app.get('/:route(Search|Random)', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'))
+})
+
+// const headers: Object = {
+//   headers: {
+//     Authorization: ` ${process.env.FINHUB_API_KEY}`,
+//   },
+// }
+
+app.get(
+  '/api/stock/search/:searchInput',
+  async (req: Request, res: Response) => {
+    try {
+      const { stockSearchInput } = req.params
+      const response = await axios.get(
+        `https://financialmodelingprep.com/api/v3/search?query=${stockSearchInput}&limit=10&exchange=NASDAQ&apikey=${process.env.FMP_API_KEY}`
+
+        // headers
+      )
+
+      const data = response.data
+      res.json(data)
+      console.log(response.data)
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ error: 'Internal server error' })
+    }
+  }
+)
