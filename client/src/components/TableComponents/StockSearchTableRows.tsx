db79e82a4ff62400c31f7a74aae1a08c9ddbf476
Kurtis <kcasperson7@gmail.com>
2023-07-12 23:35:17 -0700

    remove comments

--- client/src/components/TableComponents/StockSearchTableRows.tsx
+++ client/src/components/TableComponents/StockSearchTableRows.tsx
@@ -14,13 +14,7 @@ const StockSearchTableRows  = ({stockResultData}: Props) => {
             <td>{stock.name}</td>
             <td>{stock.symbol}</td>
            <td>
-            {/* <button className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-2 rounded-full"
-            // when button is clicked a trading modal appears
-            // how to make a modal appear 
-            onClick={() => {setShow(true), console.log('click')}}
-            ></button>
-            {show && <TradeModal/>} */}
-            {<TradeModal stockSymbol={stock.symbol}/>}
+            <TradeModal stockSymbol={stock.symbol}/>
             </td>
           </tr>
         ))}
