653a5a77d6115def0b220890a9704e02060505a1
Kurtis <kcasperson7@gmail.com>
2023-08-12 15:45:55 -0700

    Add table to home page

--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,8 +1,45 @@
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
+import MarketNewsTable from './MarketNewsTable';
 
 
 const HomePage = () => {
+  const [marketNews, setMarketNews] = useState([])
+
+
+  useEffect(()  => {
+  async function getMarketNews(){
+    try {
+      const response = await axios.get('/api/marketNews')
+      console.log(response.data)
+      setMarketNews(response.data)
+      
+     
+    } catch (error) {
+      console.error(error)
+    }
+  
+  }
+  getMarketNews()
+  },[])
+
+  const marketNewsTable = marketNews.map(( news) => {
+    return (
+      <MarketNewsTable
+        key={news['id']}
+        news={news}
+      />
+    )
+  })
+
+
+
+  
   return (
+    <>
     <div>HomePage</div>
+    <div id="newsTable">{marketNewsTable}</div>
+    </>
   )
 }
 
