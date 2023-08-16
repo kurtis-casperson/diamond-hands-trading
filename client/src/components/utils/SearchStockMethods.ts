2985aa85dd49e9d1a74fef6e27ca673dc993c9c0
Kurtis <kcasperson7@gmail.com>
2023-08-16 14:19:23 -0700

    add util stock price method

--- client/src/components/utils/SearchStockMethods.ts
+++ client/src/components/utils/SearchStockMethods.ts
@@ -1,13 +1,21 @@
 import axios from 'axios'
 
-class StockDataMethods {
-  searchStock = async (searchQuery: string) => {
-    try {
-      const response = await axios.get(`/api/stock/search/${searchQuery}`)
-      return response.data
-    } catch (error) {
-      console.error(error)
-    }
+const searchStock = async (searchQuery: string) => {
+  try {
+    const response = await axios.get(`/api/stock/search/${searchQuery}`)
+    return response.data
+  } catch (error) {
+    console.error(error)
   }
 }
-export default StockDataMethods
+
+const fetchStockPrice = async (stockSymbol: string) => {
+  try {
+    const response = await axios.get(`/api/stock/price/${stockSymbol}`)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { searchStock, fetchStockPrice }
