70759c87af96b4efe4a41f97097e48d75fbecc58
Kurtis <kcasperson7@gmail.com>
2023-09-12 09:36:22 -0700

    Add context api to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import HomePage from './components/HomePage/HomePage'
 import TradePage from './components/TradePage/TradePage'
 import Login from './components/Login/LoginPage'
 import Portfolio from './components/Portfolio/Portfolio'
-// import { UserContextType, jwtUserContext } from '../src/utils/userContextMethods'
+import { UserContextType, UserContext } from '../src/utils/UserContextMethods'
 import { Route, Routes } from 'react-router-dom'
 import { useState, useContext } from 'react'
 import './App.css'
@@ -12,21 +12,21 @@ import './App.css'
 function App() {
   // trying to replace this state with context
   // should createContext and useState have the same default values?
-const [user, setUser] = useState(null)
+// const [user, setUser] = useState(null)
 // const userfunc = (user: UserContextType) => {}
 
   return (
     <>
-    {/* <jwtUserContext.Provider value={user} > */}
-      <NavBar user={user} setUser={setUser} />
+    <UserContext.Provider value={useContext(UserContext)} >
+      <NavBar />
 
       <Routes>
-        <Route path="/" element={<Login setUser={setUser}/>} />
+        <Route path="/" element={<Login/>} />
         <Route path="/Home" element={<HomePage />} />
-        <Route path="/Trade" element={<TradePage user={user}/>} />
+        <Route path="/Trade" element={<TradePage />} />
         <Route path="/Portfolio" element={<Portfolio />} />
       </Routes>
-    {/* </jwtUserContext.Provider> */}
+    </UserContext.Provider>
   </>
   )
 }
