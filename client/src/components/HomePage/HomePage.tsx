9cbf598aa4c6c9cb4b89b9a822e399ae41dbb621
Kurtis <kcasperson7@gmail.com>
2023-09-19 13:24:53 -0700

    test navigate button

--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,45 +1,48 @@
-import  { useState, useEffect } from 'react';
-import axios from 'axios';
-import MarketNewsTable from './MarketNewsTable';
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import MarketNewsTable from './MarketNewsTable'
+import { UserContext } from '../../utils/UserContextMethods'
+import { useContext } from 'react'
 
 const HomePage = () => {
   const [marketNews, setMarketNews] = useState([])
+  const userContext = useContext(UserContext)
+  const navigate = useNavigate()
+  useEffect(() => {
+    async function getMarketNews() {
+      try {
+        const response = await axios.get('/api/marketNews')
 
-
-  useEffect(()  => {
-  async function getMarketNews(){
-    try {
-      const response = await axios.get('/api/marketNews')
-      
-      setMarketNews(response.data)
-      
-     
-    } catch (error) {
-      console.error(error)
+        setMarketNews(response.data)
+      } catch (error) {
+        console.error(error)
+      }
     }
-  
-  }
-  getMarketNews()
-  },[])
-
-  const marketNewsTable = marketNews.map(( news) => {
-    return (
-      <MarketNewsTable
-        key={news['id']}
-        news={news}
-      />
-    )
+    getMarketNews()
+  }, [])
+  console.log(userContext, ' context home page')
+  const marketNewsTable = marketNews.map((news) => {
+    return <MarketNewsTable key={news['id']} news={news} />
   })
 
-
-
-  
   return (
     <>
-    <div>HomePage</div>
-    <h3 className='text-gray-600  text-left pl-10'>News that's leading the Market</h3>
-    <div id="newsTable" className='grid lg:grid-cols-4 gap-10 p-6 text-left '>{marketNewsTable}</div>
+      <div>HomePage</div>
+      <h3 className="text-gray-600  text-left pl-10">
+        News that's leading the Market
+      </h3>
+      <div id="newsTable" className="grid lg:grid-cols-4 gap-10 p-6 text-left ">
+        {marketNewsTable}
+      </div>
+      <button
+        className="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring mr-4"
+        onClick={() => {
+          navigate('/Trade')
+        }}
+      >
+        Navigate
+      </button>
     </>
   )
 }
--- client/src/components/TradePage/TradePage.tsx
+++ client/src/components/TradePage/TradePage.tsx
@@ -1,32 +1,33 @@
 import { FC, useState } from 'react'
 import SearchBar from './SearchBar'
 import StockTableHeader from '../TableComponents/StockTableHeader'
-
-
+import { UserContext } from '../../utils/UserContextMethods'
+import { useContext } from 'react'
 
 const TradePage: FC = () => {
-const [searchQuery, setSearchQuery] = useState<string>('')
-const [stockResultData, setStockResultData] = useState([])
-const [portfolioValue, setPortfolioValue] = useState(100000)
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const [stockResultData, setStockResultData] = useState([])
+  const [portfolioValue, setPortfolioValue] = useState(100000)
+  const userContext = useContext(UserContext)
 
-    return (
-<>
+  console.log(userContext, ' context trade page')
 
+  return (
+    <>
       <div>Trade</div>
       <SearchBar
-      searchQuery={searchQuery}
-      setSearchQuery={setSearchQuery}
-      stockResultData={stockResultData}
-      setStockResultData={setStockResultData}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        stockResultData={stockResultData}
+        setStockResultData={setStockResultData}
       />
       <StockTableHeader
-     stockResultData={stockResultData}
-     portfolioValue={portfolioValue}
-     setPortfolioValue={setPortfolioValue}
-        />
-        
-</>
-    )
-  }
-  
-  export default TradePage
+        stockResultData={stockResultData}
+        portfolioValue={portfolioValue}
+        setPortfolioValue={setPortfolioValue}
+      />
+    </>
+  )
+}
+
+export default TradePage
