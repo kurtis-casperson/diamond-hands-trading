b2fa6fa4fa6337f0ce9897cac1d563ea443ad117
Kurtis <kcasperson7@gmail.com>
2023-09-18 23:49:05 -0700

    Add user context type

--- client/src/utils/UserContextMethods.ts
+++ client/src/utils/UserContextMethods.ts
@@ -1,9 +1,15 @@
 import { createContext } from 'react'
 
-export type UserContextType = {
+export type UserContext = {
   email: string
   password: string
   id: number
+  exp: number
+}
+
+export type UserContextType = {
+  user: UserContext | null
+  setUser: React.Dispatch<React.SetStateAction<UserContextType | null>>
 }
 
 const UserContext = createContext<UserContextType | null>(null)
