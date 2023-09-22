f2793470759176ff73cc2aba48c2084fea91416d
Kurtis <kcasperson7@gmail.com>
2023-09-22 01:10:36 -0700

    Add get request method for portfolio table

--- client/src/components/Portfolio/PortfolioTable.tsx
+++ client/src/components/Portfolio/PortfolioTable.tsx
@@ -1,31 +1,28 @@
-
+import axios from 'axios'
 import { Table } from 'react-bootstrap'
-import  { useEffect } from 'react';
+import { useEffect } from 'react'
 import { TableData } from './type'
 
 type Props = {
-    tableData: TableData
-    setTableData: (value: TableData) => void;
+  tableData: TableData
+  setTableData: (value: TableData) => void
 }
 
-
-const PortfolioTable = ({tableData, setTableData }: Props) => {
-
-
+const PortfolioTable = ({ setTableData }: Props) => {
   useEffect(() => {
-    fetchData();
-  }, [tableData]);
+    fetchData()
+  }, [])
 
   const fetchData = async () => {
     try {
-      const response = await fetch('/api/data'); 
-      const jsonData = await response.json();
-      setTableData(jsonData);
+      const response = await axios.get('/api/data')
+      return response
+      // const jsonData = await response
+      // setTableData()
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error)
     }
-  };
-
+  }
 
   return (
     <>
@@ -35,14 +32,12 @@ const PortfolioTable = ({tableData, setTableData }: Props) => {
             <th>Company</th>
             <th>Symbol</th>
             <th>Value</th>
-            <th>Trade</th>
+            <th>Change +/-</th>
           </tr>
         </thead>
-
       </Table>
     </>
   )
 }
 
 export default PortfolioTable
-
