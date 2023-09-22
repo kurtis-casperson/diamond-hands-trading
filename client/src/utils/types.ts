4d8cd6add82f028680a02c9aefdae78c60880be1
Kurtis <kcasperson7@gmail.com>
2023-09-21 23:10:00 -0700

    Add types file

---
+++ client/src/utils/types.ts
@@ -0,0 +1,11 @@
+export type UserContext = {
+  email: string
+  password: string
+  userID: number
+  exp: number
+}
+
+export type UserContextType = {
+  user: UserContext | null
+  setUser: React.Dispatch<React.SetStateAction<UserContext | null>>
+}
