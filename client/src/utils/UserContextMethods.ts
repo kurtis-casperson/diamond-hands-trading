12aaaad8521be7ea0ebadb4f340d9a2dbd3d5c55
Kurtis <kcasperson7@gmail.com>
2023-09-23 01:09:59 -0700

    set default values

--- client/src/utils/UserContextMethods.ts
+++ client/src/utils/UserContextMethods.ts
@@ -1,5 +1,10 @@
 import { createContext } from 'react'
 import { UserContextType, UserContext } from './types'
+import Cookies from 'universal-cookie'
+import jwt from 'jwt-decode'
+
+// const cookies = new Cookies()
+// const decodedUserCookie: any = jwt(cookies.get('jwt_authorization'))
 
 const UserContext = createContext<UserContextType | null>(null)
 
