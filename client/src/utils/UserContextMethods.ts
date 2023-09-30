4f2a12a03fb876c77ca438bf954ef4e3755b1b67
Kurtis <kcasperson7@gmail.com>
2023-09-29 18:22:16 -0700

    local storage

--- client/src/utils/UserContextMethods.ts
+++ client/src/utils/UserContextMethods.ts
@@ -1,10 +1,10 @@
 import { createContext } from 'react'
 import { UserContextType, UserContext } from './types'
 
-const getUser = localStorage.getItem('user')
+const getUser: any = localStorage.getItem('user')
 const user = JSON.parse(getUser || '{}') || null
 
-const UserContext = createContext<UserContextType>(user)
+const UserContext = createContext<UserContextType | null>(user)
 
 export const UserProvider = UserContext.Provider
 
