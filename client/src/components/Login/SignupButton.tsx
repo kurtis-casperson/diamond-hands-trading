8e7f21e58e5ddeff93a202c32ba894c9788b9769
Kurtis <kcasperson7@gmail.com>
2023-10-16 21:18:16 -0700

    remove unused code

--- client/src/components/Login/SignupButton.tsx
+++ client/src/components/Login/SignupButton.tsx
@@ -1,78 +1,59 @@
-
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {
-    
-    email: string;
-    password: string
-  
+  email: string
+  password: string
 }
 
+const SignupButton = ({ email, password }: Props) => {
+  const navigate = useNavigate()
 
-
-const SignupButton = ({email,password }: Props) => {
-  const navigate = useNavigate(); 
-  
-    const handleSignup = async (email: string, password: string, event: any) => {
-      event.preventDefault();
-      if(userValidation(email, password)){
-      try{
-        let response =  await axios.post('/api/signup', {
+  const handleSignup = async (email: string, password: string, event: any) => {
+    event.preventDefault()
+    if (userValidation(email, password)) {
+      try {
+        let response = await axios.post('/api/signup', {
           user_email: email,
-          user_password: password
-          
+          user_password: password,
         })
-        if(response.status === 201){
+        if (response.status === 201) {
           alert('you signed up successfully')
-          navigate('/Home');
+          navigate('/')
         }
-       
-      } catch(error: any){
-        if(error.response.status === 501){
+      } catch (error: any) {
+        if (error.response.status === 501) {
           alert('signup error, user already exists')
         }
-    
       }
-      
-    };
-
-  }
-const userValidation = (email: string, password: string) => {
-  if (!email) {
-    return false
-   
-  }
-  if (email.includes(' ')) {
-    return false
-    // {status: false, msg:'Please remove all spaces from the email' };
-  }
-  if(!email.includes('@')) {
-    return false
-    // {status: false, msg:'Please enter a standard email format' };
-  }
-  if (!password) {
-    return false
-    // {status: false, msg: 'Please provide a password'};
-  }
-  if (password.includes(' ')) {
-      return false
-      //  {status: false, msg: 'Please remove all spaces from the password' };
     }
-  return {status: true, msg: 'valid' };
-}
-     
+  }
+  const userValidation = (email: string, password: string) => {
+    if (!email) {
+      return false
+    }
+    if (email.includes(' ')) {
+      return false
+    }
+    if (!email.includes('@')) {
+      return false
+    }
+    if (!password) {
+      return false
+    }
+    if (password.includes(' ')) {
+      return false
+    }
+    return { status: true, msg: 'valid' }
+  }
 
-
-return(
-
+  return (
     <button
       className="bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring"
       onClick={(event) => handleSignup(email, password, event)}
     >
       Sign Up
     </button>
-
-)
+  )
 }
 export default SignupButton
