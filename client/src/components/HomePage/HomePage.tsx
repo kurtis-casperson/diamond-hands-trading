fdf65a11bd0a48bc79706f6c8ce1c6cd0b05a426
Kurtis <kcasperson7@gmail.com>
2023-10-16 21:18:52 -0700

    Insert new user tradingCash request

--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import MarketNewsTable from './MarketNewsTable'
 import { UserContext } from '../../utils/UserContextMethods'
-import { useContext } from 'react'
 import { getMarketNews } from '../../utils/MarketNewsMethods'
 
 const HomePage = () => {
   const [marketNews, setMarketNews] = useState([])
+  const [cashValue, setCashValue] = useState(100000)
   const userContext = useContext(UserContext)
+  const userId = userContext?.user?.userID
   const navigate = useNavigate()
+
+  console.log('userId', userId)
 
   useEffect(() => {
     const fetchMarketNews = async () => {
@@ -17,6 +20,17 @@ const HomePage = () => {
       setMarketNews(marketNews)
     }
     fetchMarketNews()
+    const userTradingCash = async (userId: number | undefined) => {
+      try {
+        await axios.post('/api/userTradingCash', {
+          user_id: userId,
+          tradingCash: cashValue,
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    userTradingCash(userId)
   }, [])
 
   const marketNewsTable = marketNews.map((news) => {
