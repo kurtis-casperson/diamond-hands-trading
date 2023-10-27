677da6f4ff87282675a7f414575a811b6ef0f066
Kurtis <kcasperson7@gmail.com>
2023-10-26 20:09:01 -0700

    Add piechart logic

--- client/src/components/Charts/PieChart.tsx
+++ client/src/components/Charts/PieChart.tsx
@@ -1,20 +1,30 @@
 import { Chart } from 'react-google-charts'
+import { useState, useEffect } from 'react'
+type Props = {
+  holdings: any
+}
 
-type Props = {}
+const PieChart = ({ holdings }: Props) => {
+  const [data, setData] = useState<(string | number | {})[][]>([])
 
-const PieChart = () => {
+  useEffect(() => {
+    const holdingData = [['symbol', 'amount'], []]
+  }, [holdings])
+
   const options = {
     title: 'Portfolio View',
     pieHole: 0.4,
     is3D: false,
   }
-
+  if (!holdings) {
+    return ''
+  }
   return (
     <div className="mb-4 chart-container shadow-sm">
       {/* @ts-ignore */}
       <Chart
         chartType="PieChart"
-        // data={data}
+        data={holdings}
         options={options}
         width={'100%'}
         height={'400px'}
