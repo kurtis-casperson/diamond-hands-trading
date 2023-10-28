c2c6865ee71c577134b9fb3a4c4f81b1387c2280
Kurtis <kcasperson7@gmail.com>
2023-10-28 10:21:16 -0700

    fix holdings type issue

--- client/src/components/Charts/PieChart.tsx
+++ client/src/components/Charts/PieChart.tsx
@@ -1,14 +1,29 @@
 import { Chart } from 'react-google-charts'
 import { useState, useEffect } from 'react'
+import { PortfolioDataType } from '../../utils/types'
 type Props = {
-  holdings: any
+  holdings: PortfolioDataType
 }
 
 const PieChart = ({ holdings }: Props) => {
   const [data, setData] = useState<(string | number | {})[][]>([])
-
+  console.log(holdings)
+  // const newArray = new Array<PortfolioDataType>()
+  // console.log('newArray', newArray)
   useEffect(() => {
-    const holdingData = [['symbol', 'amount'], []]
+    const holdingData = [['symbol', 'value']]
+    // for (let holding in holdings) {
+    //   const { stockSymbol, stockValue } = holding
+
+    //   holdingData.push([stockSymbol, stockValue])
+    //   console.log(holdingData)
+    // }
+    // TODO
+    // change this to an array of objects
+    const { stockSymbol, stockValue } = holdings
+    holdingData.push([stockSymbol.toString(), stockValue.toString()])
+    console.log('holdingData', holdingData)
+    setData(holdingData)
   }, [holdings])
 
   const options = {
@@ -17,6 +32,7 @@ const PieChart = ({ holdings }: Props) => {
     is3D: false,
   }
   if (!holdings) {
+    // error loading chart message
     return ''
   }
   return (
@@ -24,7 +40,7 @@ const PieChart = ({ holdings }: Props) => {
       {/* @ts-ignore */}
       <Chart
         chartType="PieChart"
-        data={holdings}
+        data={data}
         options={options}
         width={'100%'}
         height={'400px'}
