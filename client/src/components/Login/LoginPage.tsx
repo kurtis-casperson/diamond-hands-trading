3b2c70bc8e45aa30d069e0c983810d20e52efa0a
Kurtis <kcasperson7@gmail.com>
2023-11-04 00:03:04 -0700

    Add styles

--- client/src/components/Login/LoginPage.tsx
+++ client/src/components/Login/LoginPage.tsx
@@ -31,21 +31,31 @@ const LoginPage = () => {
   return (
     <>
       <div className="justify-center ">
-        <h2 className="text-4xl tracking-widest font-bold text-center pt-20 text-black">
+        <h2 className="text-4xl tracking-widest font-extrabold text-center pt-20 text-black">
           Diamond Hand Trading
         </h2>
-        <p className="text-gray-600 pt-2 pb-20 mb-10">
+        <p className="text-gray-600 pt-2 pb-10 mb-10">
           Simulate your trades without the pressure of losing money!
           <br></br>
           Learn how to trade, without the risk!
         </p>
       </div>
+      <p className="text-2xl font-extrabold justify-center text-green-400 pb-10 ">
+        We're giving you 100k to paper trade with!
+      </p>
       <div className="flex justify-center h-screen bg-white row">
-        <div className="col-md-3 pt-20">
-          <p>Start with 10000</p>
+        <div className="col-md-3 text-left pt-15">
+          <h4 className="text-green-600">Market News</h4>
+          <p className="text-black">
+            Stay plugged in with our market news updates{' '}
+          </p>
           <h4 className="text-green-600">Portfolio</h4>
           <p className="text-black">
             Get an up-to-date view of your portfolio and track your progress{' '}
+          </p>
+          <h4 className="text-green-600">Trade</h4>
+          <p className="text-black">
+            Live price updates for your favorite stocks!{' '}
           </p>
         </div>
         <div>
