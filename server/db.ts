cffb21a3228f903148c163794e96ffbbd2efeed9
Kurtis <kcasperson7@gmail.com>
2023-12-20 04:37:37 -0800

    Create db connect file

---
+++ server/db.ts
@@ -0,0 +1,16 @@
+const { Client } = require('pg')
+
+const client = new Client({
+  host: 'database-1-diamond-trading.czggh6d9nqaf.us-west-1.rds.amazonaws.com',
+  user: 'postgres',
+  password: process.env.PGADMIN_PWD,
+  port: 5432,
+  database: 'postgres',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
+client.connect()
+
+export default client
